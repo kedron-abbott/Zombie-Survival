41d2df579605b66267e5a03344a859b1c6d4b2e6
kedron-abbott <kedronabbott@Kedrons-MacBook-Pro.local>
2016-11-15 19:13:04 -0600

    adding files

---
+++ ZombieSurvivalFinal/QuicktimeEvent.py
@@ -0,0 +1,19 @@
+import datetime, time
+
+stop = "go"
+start_time = datetime.datetime.now()
+end_time = start_time + datetime.timedelta(seconds=10)
+
+while True:
+    key = raw_input("type: ")
+    if key == "yes":
+        stop = "stop"
+
+    if stop == "stop" and datetime.datetime.now() < end_time:
+        print ('You made it!')
+        break;
+    elif stop == "stop" and datetime.datetime.now() > end_time:
+        print("you took too long!")
+        break; #how do you break out of this when time is up???
+    else:
+        print("wrong");
